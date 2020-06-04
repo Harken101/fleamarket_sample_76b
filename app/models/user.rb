aab107d352aa9fcf323b63f4279e4f74f2a9fa38
it-takashi <takashi8816@gmail.com>
2020-06-04 15:57:53 +0900

    add (devise-migarat user.rb)
    バリデーションの設定
    migarationファイルを作成した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,presence: true, uniqueness: true
+  validates :family_name,presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name,presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :furigana_family_name,presence: true,{ with: /\A[ぁ-んー－]+\z/}
+  validates :furigana_first_name,presence: true,{ with: /\A[ぁ-んー－]+\z/}
+  validates :birthday,presence: true,
+  validates :email,presence: true, uniqueness: true
 end
--- db/migrate/20200604031608_devise_create_users.rb
+++ db/migrate/20200604031608_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.string :family_name,          null: false
+      t.string :first_name,           null: false
+      t.string :furigana_family_name, null: false
+      t.string :furigana_first_name,  null: false
+      t.date :birthday,               null: false
+      t.string :email,                null: false, default: ""
+      t.string :encrypted_password,   null: false, default: ""
+      
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +43,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
