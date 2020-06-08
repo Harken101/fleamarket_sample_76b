32d244fbcc77dfef2d43f0b0e03744f63cadc375
Harken101 <guhukasutamu85@gmail.com>
2020-06-08 12:37:35 +0900

    frist-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'toppage#index'
-  resources :items, only: :new do
+  resources :items, only: [:new, :show] do
     collection do
       get :buyscreen
     end
