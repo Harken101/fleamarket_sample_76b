f9be8692d92fe6d665216a0a8349d89681157432
it-takashi <takashi8816@gmail.com>
2020-06-09 13:32:23 +0900

    add gemfile
    単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "nicknameがない場合は登録できないこと" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?(:validates_step1)
+      expect(user.errors[:nickname]).to include("を入力してください")
+    end
+  end
+end
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
