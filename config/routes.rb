fe3a7071d2aff1079b88fed53334268b3eda0b28
Harken101 <guhukasutamu85@gmail.com>
2020-06-16 14:46:38 +0900

    復旧ver1

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,34 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
   resources :users, only: [:edit, :update, :show]
   root 'items#index'
     resources :address, only: [:create,:edit, :update]
     resources :cards, only: [:create, :edit, :update]
-  resources :items  do
-    collection do
-      get :buyscreen
-      get :search
-      get :search_two
+  
+    resources :card, only: [:new, :show] do
+      collection do
+        post 'show', to: 'card#show'
+        post 'pay', to: 'card#pay'
+        post 'delete', to: 'card#delete'
+      end
     end
+
+
+    resources :items  do
+      resources :purchase, only: [:index] do
+        collection do
+          get 'index', to: 'purchase#index'
+          post 'pay', to: 'purchase#pay'
+          get 'done', to: 'purchase#done'
+        end
+      end
+
+      collection do
+        get :buyscreen
+        get :search
+        get :search_two
+      end
   end
 end
