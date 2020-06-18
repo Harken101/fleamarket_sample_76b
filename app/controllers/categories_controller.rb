392f3c3e587c6232e899c798e0b2502e2e852d78
it-takashi <takashi8816@gmail.com>
2020-06-18 17:59:33 +0900

    add categories.controller
    細かい修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,7 @@ class CategoriesController < ApplicationController
       # 孫カテゴリーを選択していた場合の処理
     elsif @category.ancestry.include?("/")
       # Categoryモデル内の親カテゴリーに紐づく孫カテゴリーのidを取得
-      @items = Item.where(category_id: params[:id])
+      @items = Item.where(grandchild_category_id: params[:id])
 
     # 子カテゴリーを選択していた場合の処理
     else
@@ -36,7 +36,7 @@ class CategoriesController < ApplicationController
 
   def find_item(category)
     category.each do |id|
-      item_array = Item.includes(:images).where(category_id: id)
+      item_array = Item.includes(:images).where(grandchild_category_id: id)
       # find_by()メソッドで該当のレコードがなかった場合、itemオブジェクトに空の配列を入れないようにするための処理
       if item_array.present?
         item_array.each do |item|
